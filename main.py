f4a4ef822a6a3c130a8325b114ad4e3babef3ead
AshwinHegde <ashwin.hegde@noble.ai>
2024-05-11 23:46:01 -0700

    cleaned up main.py

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import pathlib
 from groq import Groq
 from llama_index.core import SimpleDirectoryReader
 import colorama
+import pathlib
+from pathlib import Path
 from termcolor import colored
 from asciitree import LeftAligned
 from asciitree.drawing import BoxStyle, BOX_LIGHT
 from src.loader import get_doc_summaries
+from src.tree_generator import create_file_tree
 
 import click
 
@@ -16,68 +19,40 @@ colorama.init()  # Initializes colorama to make it work on Windows as well
 
 
 @click.command()
-@click.argument("src_path", type=click.Path(exists=True))
+@click.argument("path", type=click.Path(exists=True))
 @click.argument("dst_path", type=click.Path())
 @click.option("--auto-yes", is_flag=True, help="Automatically say yes to all prompts")
-def main(src_path, dst_path, auto_yes=False):
+def main(path, dst_path, auto_yes=False):
     os.environ["GROQ_API_KEY"] = (
         "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"
     )
 
-    summaries = get_doc_summaries(src_path)
+    summaries = get_doc_summaries(path)
 
-    for summary in summaries:
-        print(colored(summary["file_name"], "green"))  # Print the filename in green
-        print(summary["summary"])  # Print the summary of the contents
-        print("-" * 80 + "\n")  # Print a separator line with spacing for readability
-
-    # Create File Tree
-    FILE_PROMPT = """
-You will be provided with list of files and a summary of their contents. Read them carefully, then propose a directory structure that optimally organizes the files using known conventions and best practices.
-
-Use descriptive folder names. Rename poorly-named files with concise new names to reflect the contents of file.
-
-Your response must be a JSON object with the following schema:
-```json
-    {
-      "files": [
-        {
-          "filename": "proposed name of the file",
-          "path": "path under proposed directory structure, including the updated filename"
-        }
-      ]
-    }
-```
-""".strip()
-
-    client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {"role": "system", "content": FILE_PROMPT},
-            {"role": "user", "content": json.dumps(summaries)},
-        ],
-        model="llama3-8b-8192",
-        response_format={"type": "json_object"},  # Uncomment if needed
-        temperature=0,
-    )
-
-    file_tree = json.loads(chat_completion.choices[0].message.content)["files"]
+    # Get file tree
+    print(summaries)
+    files = create_file_tree(summaries)
 
     BASE_DIR = pathlib.Path(dst_path)
     BASE_DIR.mkdir(exist_ok=True)
 
     # Recursively create dictionary from file paths
     tree = {}
-    for file in file_tree:
-        parts = pathlib.Path(file["path"]).parts
+    for file in files:
+        parts = Path(file["dst_path"]).parts
         current = tree
         for part in parts:
             current = current.setdefault(part, {})
 
-    tree = {BASE_DIR: tree}
+    tree = {path: tree}
 
     tr = LeftAligned(draw=BoxStyle(gfx=BOX_LIGHT, horiz_len=1))
-    click.echo(tr(tree))
+    print(tr(tree))
+
+    # Prepend base path to dst_path
+    for file in files:
+        file["dst_path"] = os.path.join(path, file["dst_path"])
+        file['summary'] = summaries[files.index(file)]['summary']
 
     if not auto_yes and not click.confirm(
         "Proceed with directory structure?", default=True
@@ -85,8 +60,8 @@ Your response must be a JSON object with the following schema:
         click.echo("Operation cancelled.")
         return
 
-    for file in file_tree:
-        file["path"] = pathlib.Path(file["path"])
+    for file in files:
+        file["path"] = pathlib.Path(file["dst_path"])
         # Create file in specified base directory
         (BASE_DIR / file["path"]).parent.mkdir(parents=True, exist_ok=True)
         with open(BASE_DIR / file["path"], "w") as f:
